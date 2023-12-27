09d8c73f9ddcb870bf5c096bf6086b6043435fbc
b4ld3gn1k <foxyincube@icloud.com>
2023-12-27 13:44:48 +0500

    version 0.1 Происходит запуск генерируется число покупок при нажатии прибавляется 1 еденица товара при достижении необхлдимого колличества кнопка исчезает и всплывает уведомление о купленном нужном колличестве

--- app/src/main/java/com/example/productapp/MainActivity.java
+++ app/src/main/java/com/example/productapp/MainActivity.java
@@ -4,57 +4,116 @@ import android.annotation.SuppressLint;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    private static int countClick;
-    private static int randomNumber;
+    private Button breadButton;
+    private TextView breadClickCount;
+    private TextView breadCount;
 
-    @SuppressLint("SetTextI18n")
+    private Button milkButton;
+    private TextView milkClickCount;
+    private TextView milkCount;
+
+    private Button eggsButton;
+    private TextView eggsClickCounnt;
+    private TextView eggsCount;
+
+    private Button cheesButton;
+    private TextView cheesClickCount;
+    private TextView cheesCount;
+
+    private Button sausageButton;
+    private TextView sausageClickCount;
+    private TextView sausageCount;
+
+    static int clickBread = 0;
+    static int clickMilk = 0;
+    static int clickEggs = 0;
+    static int clickChees = 0;
+    static int clickSausage = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
 
-        randomCount();
+        setValue();
 
-        TextView rndCount = findViewById(R.id.textView);
-        rndCount.setText(Integer.toString(randomNumber));
+        breadButton = findViewById(R.id.breadButton);
+        milkButton = findViewById(R.id.milkButton);
+        eggsButton = findViewById(R.id.eggsButton);
+        cheesButton = findViewById(R.id.chessButton);
+        sausageButton = findViewById(R.id.sausageButton);
 
-        TextView startPlusCount = findViewById(R.id.textView2);
-        startPlusCount.setText("0");
-        
     }
 
     @SuppressLint("SetTextI18n")
-    public void onClick(View view){
-        TextView rndCount = findViewById(R.id.textView);
-        TextView plusCount = findViewById(R.id.textView2);
-
-        if (countClick == randomNumber){
-
-            countClick = 0;
-
-            rndCount.setText(Integer.toString(randomCount()));
-            plusCount.setText("0");
-        } else {
-            plusCount.setText(Integer.toString(incrementCount()));
+    public void onClick(View v) {
+        if (v.getId() == breadButton.getId()){
+            breadClickCount.setText(Integer.toString(++clickBread));
+            countEqualsClick(breadCount, v, breadClickCount , breadButton);
+        } else if (v.getId() == milkButton.getId()) {
+            milkClickCount.setText(Integer.toString(++clickMilk));
+            countEqualsClick(milkCount, v, milkClickCount , milkButton);
+        }else if (v.getId() == eggsButton.getId()) {
+            eggsClickCounnt.setText(Integer.toString(++clickEggs));
+            countEqualsClick(eggsCount, v, eggsClickCounnt , eggsButton);
+        }else if (v.getId() == cheesButton.getId()) {
+            cheesClickCount.setText(Integer.toString(++clickChees));
+            countEqualsClick(cheesCount, v, cheesClickCount , cheesButton);
+        }else if (v.getId() == sausageButton.getId()) {
+            sausageClickCount.setText(Integer.toString(++clickSausage));
+            countEqualsClick(sausageCount, v, sausageClickCount , sausageButton);
         }
     }
 
-    private int incrementCount(){
-        return ++countClick;
+    @SuppressLint("SetTextI18n")
+    private void setValue() {
+        breadCount = findViewById(R.id.breadCount);
+        breadCount.setText(Integer.toString(randomCount()));
+        breadClickCount = findViewById(R.id.breadClickCount);
+        breadClickCount.setText("0");
+
+        milkCount = findViewById(R.id.milkCount);
+        milkCount.setText(Integer.toString(randomCount()));
+        milkClickCount = findViewById(R.id.milkClickCount);
+        milkClickCount.setText("0");
+
+        eggsCount = findViewById(R.id.eggsCounnt);
+        eggsCount.setText(Integer.toString(randomCount()));
+        eggsClickCounnt = findViewById(R.id.eggsClickCount);
+        eggsClickCounnt.setText("0");
+
+        cheesCount = findViewById(R.id.cheesCount);
+        cheesCount.setText(Integer.toString(randomCount()));
+        cheesClickCount = findViewById(R.id.cheesClickCount);
+        cheesClickCount.setText("0");
+
+        sausageCount = findViewById(R.id.sausageCount);
+        sausageCount.setText(Integer.toString(randomCount()));
+        sausageClickCount = findViewById(R.id.sausageClickCount);
+        sausageClickCount.setText("0");
     }
 
-    private int randomCount(){
+    private void countEqualsClick(TextView count, View view, TextView click, Button button){
+        if (count.getText().equals(click.getText())){
+            button.setVisibility(View.GONE);
+            count.setVisibility(View.GONE);
+            click.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, "Нужное колличесво " + button.getText() + " куплено.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private int randomCount() {
         Random random = new Random();
         int max = 10;
         int min = 1;
-        return randomNumber = random.nextInt(max - min + 1) + min;
+        return random.nextInt(max - min + 1) + min;
     }
-
 }
